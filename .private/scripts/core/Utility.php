2121b23ef8019cd0b8339681a462cb42530e1384
Vicary Archangel <denvil@gmail.com>
2013-05-30 20:39:59 +0800

    Added utility method objectToArray()

--- .private/scripts/core/Utility.php
+++ .private/scripts/core/Utility.php
@@ -239,6 +239,21 @@ class Utility {
   }
 
   /**
+   * Deep conversion from stdClass (object) to array.
+   */
+  static function objectToArray($data) {
+    if ( is_object($data) ) {
+      $data = get_object_vars($data);
+    }
+
+    if ( is_array($data) ) {
+      return array_map(array(__CLASS__, __FUNCTION__), $data);
+    }
+
+    return $data;
+  }
+
+  /**
    * Flatten an array, concatenating keys with specified delimiter.
    */
   static function flattenArray(&$input, $delimiter = '.', $flattenNumeric = TRUE) {
