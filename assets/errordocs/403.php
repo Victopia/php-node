77f7af7acf24caa101ad73605052ed5cf96bdc74
Vicary Archangel <denvil@gmail.com>
2013-12-06 19:08:31 +0800

    More default error documents.

---
+++ assets/errordocs/403.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>403 Forbidden | <?php echo @$_SERVER['HTTP_HOST']; ?></title>
+	</head>
+
+	<body>
+		<h1>403 Forbidden</h1>
+
+		<p>Access to requested resource is forbidden by the server.</p>
+
+		<hr />
+
+		<sub>Gateway repsonse.</sub>
+	</body>
+</html>
--- assets/errordocs/405.php
+++ assets/errordocs/405.php
@@ -3,13 +3,13 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>405 Forbidden | <?php echo $_SERVER['HTTP_HOST']; ?></title>
+		<title>405 Method Not Allowed | <?php echo @$_SERVER['HTTP_HOST']; ?></title>
 	</head>
 
 	<body>
 		<h1>405 Forbidden</h1>
 
-		<p>Access to requested resource is forbidden by the server.</p>
+		<p>Method of request to target resources is not supported by the server.</p>
 
 		<hr />
 
