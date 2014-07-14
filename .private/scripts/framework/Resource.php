5d75b170457aac26a39181c4066ccf5009ca4c2e
Vicary Archangel <denvil@gmail.com>
2014-07-14 14:36:15 +0800

    New Resource logic.
    Resource new gets from database with specified locale and identifier
    string.

--- .private/scripts/framework/Resource.php
+++ .private/scripts/framework/Resource.php
@@ -7,51 +7,129 @@
 namespace framework;
 
 class Resource {
-  
+  private $localeChain;
+
+  private $localeCache = array();
+
+  //--------------------------------------------------
+  //
+  //  Constructor
+  //
+  //--------------------------------------------------
+
+  public function __construct($localeChain = 'en_US') {
+    if ( !is_array($localeChain) ) {
+      $localeChain = preg_split('/\s*,\s*/', trim($localeChain));
+    }
+
+    $this->localeChain = $localeChain;
+  }
+
   //--------------------------------------------------
   //
   //  Methods
   //
   //--------------------------------------------------
-  
+
   /**
    * Set a locale object.
    */
-  public static function 
-  /* void */ set($key, $value, $locale = NULL) {
-    $contents = Array(
+  public function
+  /* void */ set($key, $value, $locale = null) {
+    $contents = array(
       NODE_FIELD_COLLECTION => 'Resource',
       'contents' => $value
     );
-    
-    if ($locale !== NULL) {
+
+    if ( $locale !== null ) {
       $contents['locale'] = $locale;
     }
-    
-    return Node::set($contents);
+    elseif ( $this->localeChain ) {
+      $localeChain = (array) $this->localeChain;
+
+      $contents['locale'] = end($localeChain);
+
+      unset($localeChain);
+    }
+
+    if ( !@$contents['locale'] ) {
+      throw new framework\exceptions\ResourceException('Invalid locale specified.');
+    }
+
+    return \node::set($contents);
   }
-  
+
+  /**
+   * Check whether a resource key is set.
+   */
+  public function __isset($key) {
+    $localeChain = (array) $this->localeChain;
+
+    $cache = &$this->localeCache[$key];
+
+    // Search the cache based on locale chain.
+    if ( !$cache ) {
+      $this->ensureCache($key);
+    }
+
+    foreach ( $localeChain as $locale ) {
+      if ( @$cache[$locale] ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   /**
    * Retrieve a locale object.
    */
-  public static function 
-  /* Mixed */ get($key, $locale = NULL) {
-    $filter = Array(
-      NODE_FIELD_COLLECTION => 'Resource',
-      'contents' => $value
-    );
-    
-    if ($locale !== NULL) {
-      $filter['locale'] = $locale;
+  public function
+  /* Mixed */ __get($key) {
+    $localeChain = (array) $this->localeChain;
+
+    $cache = &$this->localeCache[$key];
+
+    // Search the cache based on locale chain.
+    if ( !$cache ) {
+      $this->ensureCache($key);
     }
-    
-    $res = Node::get($filter);
-    
-    if (count($res) > 0) {
-      return $res['contents'];
+
+    foreach ( $localeChain as $locale ) {
+      if ( @$cache[$locale] ) {
+        return $cache[$locale];
+      }
     }
-    
-    return NULL;
+
+    return null;
   }
-  
+
+  //--------------------------------------------------
+  //
+  //  Private methods
+  //
+  //--------------------------------------------------
+
+  /**
+   * @private
+   *
+   * Add resources of specified key into cache.
+   */
+  private function ensureCache($key) {
+    $cache = &$this->localeCache[$key];
+
+    $res = \core\Node::get(array(
+        NODE_FIELD_COLLECTION => FRAMEWORK_COLLECTION_RESOURCES
+      , 'identifier' => $key
+      , 'locale' => $this->localeChain
+      ));
+
+    if ( $res ) {
+      $cache = array();
+
+      array_walk($res, function($resource) use(&$cache) {
+        $cache[$resource['locale']] = json_decode($resource['value'], 1);
+      });
+    }
+  }
 }
