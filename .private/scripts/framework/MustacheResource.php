e87ce32a2ab9cdf43bea3d5f4e0668fc927a9ede
Vicary Archangel <denvil@gmail.com>
2014-07-14 14:37:52 +0800

    Binding Resource to Mustache templates.

---
+++ .private/scripts/framework/MustacheResource.php
@@ -0,0 +1,81 @@
+<?php
+/*! Resource.php
+ *
+ *  Universal access to locale based resources.
+ */
+
+namespace framework;
+
+/**
+ * This class implements the "dot notation" way to
+ * get our locale resources from database.
+ */
+class MustacheResource {
+  private $resourceInstance;
+
+  private $path;
+
+  //--------------------------------------------------
+  //
+  //  Constructor
+  //
+  //--------------------------------------------------
+
+  /**
+   * @param $locale_resource Either a string of locale chain,
+   *                         or a cascading resource object.
+   *
+   * @param $path Stored path to target resources.
+   */
+  public function __construct($locale_resource = 'en_US', $path = '') {
+    if ( $locale_resource instanceof Resource ) {
+      $this->resourceInstance = $locale_resource;
+    }
+    else {
+      $this->resourceInstance = new Resource($locale_resource);
+    }
+
+    $this->path = $path;
+  }
+
+  /**
+   * The context resolve mechanism of mustache require __isset()
+   * to be like this.
+   */
+  public function __isset($name) {
+    return true;
+  }
+
+  public function __get($name) {
+    $name = implode('.', array_filter(array($this->path, $name)));
+
+    return new MustacheResource($this->resourceInstance, $name);
+  }
+
+  /**
+   * Other methods never get the resource key directly,
+   * instead it relies on this __toString() method as a
+   * last-second-resolving.
+   */
+  public function __toString() {
+    $value = $this->resourceInstance->{$this->path};
+
+    if ( is_array($value) ) {
+      $flattenArray = function($input) use(&$flattenArray) {
+        foreach ( $input as $value ) {
+          if ( is_array($value) ) {
+            $value = $flattenArray($value);
+          }
+        }
+
+        return implode(', ', $input);
+      };
+
+      $value = $flattenArray($value);
+
+      unset($flattenArray);
+    }
+
+    return (string) $value;
+  }
+}
--- .private/scripts/resolvers/TemplateResolver.php
+++ .private/scripts/resolvers/TemplateResolver.php
@@ -10,32 +10,91 @@
 namespace resolvers;
 
 class TemplateResolver implements \framework\interfaces\IRequestResolver {
-	//--------------------------------------------------
+  private static $mustache;
+
+  private $resouces;
+
+  //----------------------------------------------------------------------
+  //
+  //  Constructor
+  //
+  //----------------------------------------------------------------------
+
+  public function __construct($localeChain = 'en_US') {
+    if ( !self::$mustache ) {
+      self::$mustache = new \Mustache_Engine();
+    }
+
+    $this->resources = new \framework\MustacheResource($localeChain);
+  }
+
+  //----------------------------------------------------------------------
+  //
+  //  Properties
+  //
+  //----------------------------------------------------------------------
+
+  private $directoryIndex = array();
+
+  public function directoryIndex($value = null) {
+    if ( is_null($value) ) {
+      return $value;
+    }
+    else {
+      $this->directoryIndex = preg_split('/[\s+,;]+/', $value);
+    }
+  }
+
+  //----------------------------------------------------------------------
 	//
 	//  Methods: IPathResolver
 	//
-	//--------------------------------------------------
+	//----------------------------------------------------------------------
 
-	public
-	/* Boolean */ function canResolve($path) {
-		return file_exists(sprintf(FRAMEWORK_TEMPLATE_PATH, $path));
-	}
+  public /* Boolean */
+  function resolve($path) {
+    // Normalize request URL to a relative path
+    if ( strpos($path, '.') !== 0 ) {
+      $path = ".$path";
+    }
 
-	public
-	/* String */ function resolve($path) {
-		$res = sprintf(FRAMEWORK_TEMPLATE_PATH, $path);
+    // Mimic directory index
+    if ( is_dir($path) ) {
+      foreach ( $this->directoryIndex as $dirIndex ) {
+        $files = glob($path . DIRECTORY_SEPARATOR . "$dirIndex.*");
 
-		if (!file_exists($res)) {
-			return FALSE;
-		}
+        $files = reset($files);
 
-		$res = file_get_contents($res);
+        if ( $files ) {
+          $path = realpath($files);
 
-		$res = new Template($res);
+          break;
+        }
+      }
 
-		//TODO: Data/View
-		$res = Data::getView(framework\Session::current());
+      unset($dirIndex, $files);
+    }
 
-		$res->render();
+    // Check whether target is a template file
+
+    /* Note @ 11 Feb, 2014
+       We support mustache only.
+    */
+    if ( file_exists($path) ) {
+      $ext = pathinfo($path, PATHINFO_EXTENSION);
+
+      switch ( $ext ) {
+        case 'mustache':
+        case 'html':
+          return $this->handle($path);
+      }
+    }
+
+    return false;
+  }
+
+	public /* String */
+  function handle($path) {
+    echo self::$mustache->render(file_get_contents($path), $this->resources);
 	}
 }
