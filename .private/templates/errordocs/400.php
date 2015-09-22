d287deb663047cfedb5ab58b856b9cd12e29aae2
Vicary Archangel <vicrry@yahoo.com.hk>
2015-09-22 17:02:04 +0800

    Add more default status documents

---
+++ .private/templates/errordocs/400.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>400 Bad Request | <?php echo framework\System::getHostname() ?></title>
+  </head>
+
+  <body>
+    <h1>400 Bad Request</h1>
+
+    <p>Requested resource cannot be handled by the server.</p>
+
+    <hr />
+
+    <sub>Gateway repsonse.</sub>
+  </body>
+</html>
---
+++ .private/templates/errordocs/502.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>502 Bad Gateway | <?php echo framework\System::getHostname() ?></title>
+  </head>
+
+  <body>
+    <h1>502 Bad Gateway</h1>
+
+    <p>Upstream server has an invalid repsonse to requested resource, please try again later.</p>
+
+    <hr />
+
+    <sub>Gateway repsonse.</sub>
+  </body>
+</html>
