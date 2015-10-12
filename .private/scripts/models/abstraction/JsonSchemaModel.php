7012e669c811a3b68e9bc2b6499ce8727b6da35b
Vicary Archangel <vicrry@yahoo.com.hk>
2015-10-12 18:30:40 +0800

    better integrate with Jsv4

--- .private/scripts/models/abstraction/JsonSchemaModel.php
+++ .private/scripts/models/abstraction/JsonSchemaModel.php
@@ -12,18 +12,31 @@ use Jsv4;
 
 abstract class JsonSchemaModel extends AbstractRelationModel {
 
-  public function validate(array &$errors = array()) {
-    $schema = $this->schema();
+  /**
+   * @protected
+   *
+   * Schema array.
+   */
+  protected $schema = array();
 
-    if ( !Jsv4::isValid($this->data, $schema) ) {
+  /**
+   * @constructor
+   *
+   * Try to read from configuration files if schema is not already set.
+   */
+  public function __construct($data = null) {
+    parent::__construct($data);
+  }
+
+  public function validate(array &$errors = array()) {
+    if ( !Jsv4::isValid($this->data, $this->schema()) ) {
       // try to coerce on initial failure
-      $result = Jsv4::coerce($this->data, $schema);
-      if ( $result->value ) {
+      $result = Jsv4::coerce($this->data, $this->schema());
+      if ( $result->valid ) {
         $this->data = $result->value;
       }
-
       // return errors if exists
-      if ( !empty($result->errors) ) {
+      else {
         $errors = array_merge(util::objectToArray($result->errors));
       }
     }
@@ -42,7 +55,25 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
         break;
     }
 
-    return conf::get("{$this->collectionName()}.model::$type", $default);
+    $ret = @$this->schema->$type;
+    if ( !$ret ) {
+      $ret = $default;
+    }
+
+    if ( !$ret && isset($this->__response) ) {
+      $this->__response->status(404);
+    }
+
+    return $ret;
+  }
+
+  function afterLoad() {
+    parent::afterLoad();
+
+    $result = Jsv4::coerce($this->data, $this->schema());
+    if ( $result->valid ) {
+      $this->data = $result->value;
+    }
   }
 
 }
