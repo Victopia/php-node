56d3bbdf6753d7825456b4cd8018258b9978e928
Vicary Archangel <vicrry@yahoo.com.hk>
2016-11-07 23:10:15 +0800

    New default routing in favour of complex resolvers.
    New resolvers like CssMinResolver can invoke a new internal request to
    ask for the original version.

--- .private/scripts/authenticators/IsInternal.php
+++ .private/scripts/authenticators/IsInternal.php
@@ -1,14 +1,24 @@
-<?php
-/* IsInternal.php | Authenticates server only processes. */
+<?php /* IsInternal.php | Authenticates server only processes. */
 
 namespace authenticators;
 
+use framework\Configuration as conf;
 use framework\Request;
+use framework\System;
 
 class IsInternal implements \framework\interfaces\IAuthenticator {
 
   static function authenticate(Request $r = null) {
-    return !$r || @$r->__local;
+    return
+      !$r ||
+      (
+        $r->client('host') &&
+        $r->client('host') == System::getHostname($r->client('secure')? 'secure': 'default')
+      ) ||
+      in_array(
+        $r->client('address'),
+        (array) conf::get('system::localhosts', '127.0.0.1')
+      );
   }
 
 }
