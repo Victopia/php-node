e9340bd140f938f122b90aa59f5eac27b3a1ff6e
Vicary Archangel <vicrry@yahoo.com.hk>
2017-02-14 18:37:03 +0800

    Adds array bug workaround for JSF.

--- .private/scripts/models/abstraction/JsonSchemaModel.php
+++ .private/scripts/models/abstraction/JsonSchemaModel.php
@@ -1,5 +1,4 @@
-<?php
-/*! JsonSchemaModel.php | Data models that leverages JSON schema. */
+<?php /*! JsonSchemaModel.php | Data models that leverages JSON schema. */
 
 namespace models\abstraction;
 
@@ -40,28 +39,55 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
     parent::__construct($data);
   }
 
+  //----------------------------------------------------------------------------
+  //
+  //  Methods
+  //
+  //----------------------------------------------------------------------------
+
+  protected $_arrayWorkaroundFields = [];
+
+  // note; Prevent array fields from wrapping multiple times during save().
+  private $isSaving = false;
+
+  //----------------------------------------------------------------------------
+  //
+  //  Methods: AbstractModel
+  //
+  //----------------------------------------------------------------------------
+
   public function validate() {
     $errors = parent::validate();
 
-    $result = Jsv4::coerce(filter($this->data), $this->schema());
+    $result = Jsv4::coerce($this->data, $this->schema());
     if ( $result->valid ) {
       $this->data = $result->value;
     }
     else {
-      $errors = array_merge(
-        $errors,
-        array_reduce(
-          $result->errors,
-          function($errors, $e) {
-            $errors[$e->getCode()] = $e->dataPath . ': ' . $e->getMessage();
-            return $errors;
-          },
-          array()
-        )
+      $errors+= array_reduce(
+        $result->errors,
+        function($errors, $e) {
+          $errors[$e->getCode()] = $e->dataPath . ': ' . $e->getMessage();
+          return $errors;
+        },
+        array()
       );
     }
 
     return $errors;
+  }
+
+  protected function populate() {
+    if ( !$this->isSaving ) {
+      // note;workaround; Wrapping plain strings in array with object { $: "string" }
+      foreach ( $this->arrayWorkaroundFields() as $field ) {
+        if ( isset($this->$field) && is_array($this->$field) ) {
+          $this->$field = array_map(wraps('$'), $this->$field);
+        }
+      }
+    }
+
+    return parent::populate();
   }
 
   public function schema($type = 'data') {
@@ -80,11 +106,44 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
       $ret = $default;
     }
 
-    if ( !$ret && isset($this->__response) ) {
-      $this->__response->status(404);
+    return $ret;
+  }
+
+  protected function beforeSave(array &$errors = array()) {
+    $this->isSaving = true;
+
+    // note;workaround; Must remove blank objects because schema form tends to create them.
+    foreach ( $this->arrayWorkaroundFields() as $field ) {
+      if ( isset($this->$field) && is_array($this->$field) ) {
+        $this->$field = filter($this->$field, compose('not', 'blank'));
+      }
     }
 
-    return $ret;
+    parent::beforeSave($errors);
+
+    if ( !$errors ) {
+      // note;workaround; Unwraps plain strings in array with object { $: "string" }
+      foreach ( $this->arrayWorkaroundFields() as $field ) {
+        if ( isset($this->$field) && is_array($this->$field) ) {
+          $this->$field = pluck($this->$field, '$');
+        }
+      }
+    }
+
+    return $this;
+  }
+
+  protected function afterSave(array &$result = null) {
+    $this->isSaving = false;
+
+    // note;workaround; Wrapping plain strings in array with object { $: "string" }
+    foreach ( $this->arrayWorkaroundFields() as $field ) {
+      if ( isset($this->$field) && is_array($this->$field) ) {
+        $this->$field = array_map(wraps('$'), $this->$field);
+      }
+    }
+
+    return parent::afterSave($result);
   }
 
   protected function afterLoad() {
