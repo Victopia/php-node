fd4601b676722294cdf25bd20df0912ef4156fb0
Vicary Archangel <vicary.archangel@member.mensa.org>
2018-04-13 17:58:22 +0800

    Removed workaround for schema-form bug.
    Also porting from `geraintluff/jsv4` to `justinrainbow/json-schema`.

--- .private/scripts/models/abstraction/JsonSchemaModel.php
+++ .private/scripts/models/abstraction/JsonSchemaModel.php
@@ -2,7 +2,9 @@
 
 namespace models\abstraction;
 
+use core\Database;
 use core\Log;
+use core\Node;
 use core\Utility as util;
 
 use framework\Configuration as conf;
@@ -21,14 +23,6 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
 
   //----------------------------------------------------------------------------
   //
-  //  Methods
-  //
-  //----------------------------------------------------------------------------
-
-  protected $_arrayWorkaroundFields = [];
-
-  //----------------------------------------------------------------------------
-  //
   //  Methods: AbstractModel
   //
   //----------------------------------------------------------------------------
@@ -39,13 +33,15 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
     // note; remove previous errors to prevent serialization of error objects.
     unset($this->{'$errors'});
 
-    $validator = new Validator();
+    $this->coerceDateTime('json-schema');
 
     $data = $this->data();
 
+    $validator = new Validator();
     $validator->validate(
       $data,
       $this->schema(),
+      Constraint::CHECK_MODE_TYPE_CAST |
       Constraint::CHECK_MODE_COERCE_TYPES |
       Constraint::CHECK_MODE_APPLY_DEFAULTS
     );
@@ -58,7 +54,7 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
       $errors+= array_reduce(
         $validator->getErrors(),
         function($errors, $e) {
-          $errors[] = $e['message'];
+          $errors[] = $e;
           return $errors;
         },
         array()
@@ -72,13 +68,15 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
     // note; remove previous errors to prevent serialization of error objects.
     unset($this->{'$errors'});
 
-    $validator = new Validator();
+    $this->coerceDateTime('json-schema');
 
     $data = $this->data();
 
+    $validator = new Validator();
     $validator->validate(
       $data,
       $this->schema(),
+      Constraint::CHECK_MODE_TYPE_CAST |
       Constraint::CHECK_MODE_COERCE_TYPES |
       Constraint::CHECK_MODE_APPLY_DEFAULTS
     );
@@ -90,7 +88,7 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
       $this->{'$errors'} = array_reduce(
         $validator->getErrors(),
         function($errors, $e) {
-          $errors[] = $e['message'];
+          $errors[] = $e;
           return $errors;
         },
         array()
@@ -113,36 +111,51 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
   }
 
   protected function beforeSave(array &$errors = array()) {
-    // note;workaround; Must remove blank objects because schema form tends to create them.
-    foreach ( $this->arrayWorkaroundFields() as $field ) {
-      if ( isset($this->$field) && is_array($this->$field) ) {
-        $this->$field = filter($this->$field, compose('not', 'blank'));
-      }
-    }
-
     parent::beforeSave($errors);
 
     if ( !$errors ) {
-      // note;workaround; Unwraps plain strings in array with object { $: "string" }
-      foreach ( $this->arrayWorkaroundFields() as $field ) {
-        if ( isset($this->$field) && is_array($this->$field) ) {
-          $this->$field = pluck($this->$field, '$');
-        }
-      }
+      $this->coerceDateTime('mysql');
     }
 
     return $this;
   }
 
-  protected function afterSave(array &$result = null) {
-    // note;workaround; Wrapping plain strings in array with object { $: "string" }
-    foreach ( $this->arrayWorkaroundFields() as $field ) {
-      if ( isset($this->$field) && is_array($this->$field) ) {
-        $this->$field = array_map(wraps('$'), $this->$field);
+  /**
+   * SUPER ugly function to convert date-time format between MySQL and JSON Schema.
+   */
+  protected function coerceDateTime($type) {
+    $fields = Node::resolveCollection($this->collectionName());
+    $fields = Database::getFields($fields, null, false);
+    $fields = filter($fields, compose(
+      matches('/(datetime|timestamp)\(\d+\)/'),
+      prop('Type')
+    ));
+    $fields = array_keys($fields);
+
+    if ( $type == 'mysql' ) {
+      // Convert RFC3339 back to MySQL DATETIME format.
+      foreach ( $fields as $key ) {
+        if ( !empty($this->$key) && is_string($this->$key) ) {
+          $this->$key = preg_replace(
+            '/^(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})(\.\d+)?(Z|([+-]\d{2}):?(\d{2}))$/',
+            '$1 $2$3',
+            $this->$key
+          );
+        }
       }
     }
-
-    return parent::afterSave($result);
+    else if ( $type == 'json-schema' ) {
+      // Coerce MySQL DATETIME format to RFC3339.
+      foreach ( $fields as $key ) {
+        if ( !empty($this->$key) && is_string($this->$key) ) {
+          $this->$key = preg_replace(
+            '/(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})(\.\d*)/',
+            '$1T$2$3Z',
+            $this->$key
+          );
+        }
+      }
+    }
   }
 
 }
