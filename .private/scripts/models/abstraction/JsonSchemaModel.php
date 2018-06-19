e8a9930677be66abcd0afa6761cec414b1e5ff8e
Vicary Archangel <vicary.archangel@member.mensa.org>
2018-06-19 23:56:50 +0800

    Supports DATETIME and TIMESTAMP without length specified in MySQL.

--- .private/scripts/models/abstraction/JsonSchemaModel.php
+++ .private/scripts/models/abstraction/JsonSchemaModel.php
@@ -33,7 +33,7 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
     // note; remove previous errors to prevent serialization of error objects.
     unset($this->{'$errors'});
 
-    $this->coerceDateTime('json-schema');
+    $this->coerceDateTime("json-schema");
 
     $data = $this->data();
 
@@ -68,7 +68,7 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
     // note; remove previous errors to prevent serialization of error objects.
     unset($this->{'$errors'});
 
-    $this->coerceDateTime('json-schema');
+    $this->coerceDateTime("json-schema");
 
     $data = $this->data();
 
@@ -100,7 +100,7 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
 
   public function schema() {
     if ( !$this->_schema ) {
-      $className = substr(strrchr(get_class($this), '\\'), 1);
+      $className = substr(strrchr(get_class($this), "\\"), 1);
 
       $this->_schema = (array) conf::get("schema.$className")->getContents();
 
@@ -114,7 +114,7 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
     parent::beforeSave($errors);
 
     if ( !$errors ) {
-      $this->coerceDateTime('mysql');
+      $this->coerceDateTime("mysql");
     }
 
     return $this;
@@ -127,29 +127,29 @@ abstract class JsonSchemaModel extends AbstractRelationModel {
     $fields = Node::resolveCollection($this->collectionName());
     $fields = Database::getFields($fields, null, false);
     $fields = filter($fields, compose(
-      matches('/(datetime|timestamp)\(\d+\)/'),
-      prop('Type')
+      matches("/(datetime|timestamp)(?:\(\d+\))?/"),
+      prop("Type")
     ));
     $fields = array_keys($fields);
 
-    if ( $type == 'mysql' ) {
+    if ( $type === "mysql" ) {
       // Convert RFC3339 back to MySQL DATETIME format.
       foreach ( $fields as $key ) {
         if ( !empty($this->$key) && is_string($this->$key) ) {
           $this->$key = preg_replace(
-            '/^(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})(\.\d+)?(Z|([+-]\d{2}):?(\d{2}))$/',
+            "/^(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})(\.\d+)?(Z|([+-]\d{2}):?(\d{2}))$/",
             '$1 $2$3',
             $this->$key
           );
         }
       }
     }
-    else if ( $type == 'json-schema' ) {
+    else if ( $type === "json-schema" ) {
       // Coerce MySQL DATETIME format to RFC3339.
       foreach ( $fields as $key ) {
         if ( !empty($this->$key) && is_string($this->$key) ) {
           $this->$key = preg_replace(
-            '/(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})(\.\d*)/',
+            "/(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})(\.\d*)/",
             '$1T$2$3Z',
             $this->$key
           );
